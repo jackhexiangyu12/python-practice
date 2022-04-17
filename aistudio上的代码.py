4919223fa19874ddb1fefeeb13fd78cb1546233f
jackhexiangyu12 <22920202204559@stu.xmu.edu.cn>
2022-04-17 10:33:27 +0800

    一稿

--- aistudio上的代码.py
+++ aistudio上的代码.py
@@ -8,7 +8,117 @@ from paddle.fluid.regularizer import L2Decay
 
 from paddle.fluid.dygraph.nn import Conv2D, BatchNorm
 from paddle.fluid.dygraph.base import to_variable
+import functools
+import paddle
 
+
+INSECT_NAMES = ['Boerner', 'Leconte', 'Linnaeus',
+                'acuminatus', 'armandi', 'coleoptera', 'linnaeus']
+
+def get_insect_names():
+    """
+    return a dict, as following,
+        {'Boerner': 0,
+         'Leconte': 1,
+         'Linnaeus': 2,
+         'acuminatus': 3,
+         'armandi': 4,
+         'coleoptera': 5,
+         'linnaeus': 6
+        }
+    It can map the insect name into an integer label.
+    """
+    insect_category2id = {}
+    for i, item in enumerate(INSECT_NAMES):
+        insect_category2id[item] = i
+
+    return insect_category2id
+
+
+def get_annotations(cname2cid, datadir):
+    filenames = os.listdir(os.path.join(datadir, 'annotations', 'xmls'))
+    records = []
+    ct = 0
+    for fname in filenames:
+        fid = fname.split('.')[0]
+        fpath = os.path.join(datadir, 'annotations', 'xmls', fname)
+        img_file = os.path.join(datadir, 'images', fid + '.jpeg')
+        tree = ET.parse(fpath)
+
+        if tree.find('id') is None:
+            im_id = np.array([ct])
+        else:
+            im_id = np.array([int(tree.find('id').text)])
+
+        objs = tree.findall('object')
+        im_w = float(tree.find('size').find('width').text)
+        im_h = float(tree.find('size').find('height').text)
+        gt_bbox = np.zeros((len(objs), 4), dtype=np.float32)
+        gt_class = np.zeros((len(objs), ), dtype=np.int32)
+        is_crowd = np.zeros((len(objs), ), dtype=np.int32)
+        difficult = np.zeros((len(objs), ), dtype=np.int32)
+        for i, obj in enumerate(objs):
+            cname = obj.find('name').text
+            gt_class[i] = cname2cid[cname]
+            _difficult = int(obj.find('difficult').text)
+            x1 = float(obj.find('bndbox').find('xmin').text)
+            y1 = float(obj.find('bndbox').find('ymin').text)
+            x2 = float(obj.find('bndbox').find('xmax').text)
+            y2 = float(obj.find('bndbox').find('ymax').text)
+            x1 = max(0, x1)
+            y1 = max(0, y1)
+            x2 = min(im_w - 1, x2)
+            y2 = min(im_h - 1, y2)
+            # 这里使用xywh格式来表示目标物体真实框
+            gt_bbox[i] = [(x1+x2)/2.0 , (y1+y2)/2.0, x2-x1+1., y2-y1+1.]
+            is_crowd[i] = 0
+            difficult[i] = _difficult
+
+        voc_rec = {
+            'im_file': img_file,
+            'im_id': im_id,
+            'h': im_h,
+            'w': im_w,
+            'is_crowd': is_crowd,
+            'gt_class': gt_class,
+            'gt_bbox': gt_bbox,
+            'gt_poly': [],
+            'difficult': difficult
+            }
+        if len(objs) != 0:
+            records.append(voc_rec)
+        ct += 1
+    return records
+# 使用paddle.reader.xmap_readers实现多线程读取数据
+def multithread_loader(datadir, batch_size= 10, mode='train'):
+    cname2cid = get_insect_names()
+    records = get_annotations(cname2cid, datadir)
+    def reader():
+        if mode == 'train':
+            np.random.shuffle(records)
+        img_size = get_img_size(mode)
+        batch_data = []
+        for record in records:
+            batch_data.append((record, img_size))
+            if len(batch_data) == batch_size:
+                yield batch_data
+                batch_data = []
+                img_size = get_img_size(mode)
+        if len(batch_data) > 0:
+            yield batch_data
+
+    def get_data(samples):
+        batch_data = []
+        for sample in samples:
+            record = sample[0]
+            img_size = sample[1]
+            img, gt_bbox, gt_labels, im_shape = get_img_data(record, size=img_size)
+            batch_data.append((img, gt_bbox, gt_labels, im_shape))
+        return make_array(batch_data)
+
+    mapper = functools.partial(get_data, )
+
+    return paddle.reader.xmap_readers(mapper, reader, 8, 10)
 # 从骨干网络输出特征图C0得到跟预测相关的特征图P0
 class YoloDetectionBlock(fluid.dygraph.Layer):
     # define YOLO-V3 detection head
